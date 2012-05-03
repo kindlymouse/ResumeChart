191e6a6cd6e84b0d14648b0186e02143c7ba09b9
kindlymouse <kindlymouse@gmail.com>
2012-05-03 21:47:19 +0800

    Index Test

--- application/views/test_jquery.php
+++ application/views/test_jquery.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php $this->load->view('global/header'); ?>
 <script>
 $(document).ready(function(){
@@ -27,3 +28,53 @@ $(document).ready(function(){
 	<button id="button">click me!</button>
 	<div class="haha"></div>
 <?php $this->load->view('global/footer'); ?>
+=======
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Test JQuery</title>
+    <?php echo $library_src; ?> 
+    <?php echo $script_foot; ?> 
+    <style>
+		.darkbar {
+			background-color: #252526;
+			}
+			
+		#topNavContainer{
+			height: 40px;
+			border-bottom-width: 1px;
+			border-bottom-style: solid;
+			border-bottom-color: #4b4b4c;
+			}		
+		#nav{
+			position:absolute;
+			bottom:10px;
+			right:10px;
+			z-index:6;
+			}
+		a{
+			color: #000000;
+			}
+		#slide-backgroup{
+			background-image:
+		}
+	</style>
+</head>
+<body>
+	<div id="nav">
+		<div class="darkbar" id="topNavContainer">
+        <div id="container_16">
+        	<div id="topNavContent">
+            	<ui id="navMenu">
+                	<li class="menu-447"><a>haha</a></li>
+                </ui>
+            </div>
+        </div>
+    </div>
+    <div id="slide-backgroup">
+    </div>
+	<div id="button">Click me!</div>
+</body>
+</html>
+>>>>>>> Index Test
