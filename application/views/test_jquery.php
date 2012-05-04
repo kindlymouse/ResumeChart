4a6f3487c969e536fc60bda1a8db09be00983e76
kindlymouse <kindlymouse@gmail.com>
2012-05-04 20:51:14 +0800

    编辑CSS文件

--- application/views/test_jquery.php
+++ application/views/test_jquery.php
@@ -1,80 +1,116 @@
-<<<<<<< HEAD
-<?php $this->load->view('global/header'); ?>
-<script>
-$(document).ready(function(){
-	$("#button").click(function(){
-		$my_name = $("#my_name").val();	
-		$.ajax({
-			type:"post",
-            data: {"my_name": $my_name},
-            //data: "my_name=" + $my_name,
-            url:"<?=base_url()?>index.php/Test_JQuery/showMyName",
-            dataType:"json",
-            accepts:"json",
-            success: function(data){
-                $('.haha').html(data.result);
-            }
-		});
-
-		/*
-		$.post('<?=base_url()?>index.php/Test_JQuery/showMyName',{my_name: $my_name},
-				function(data){
-					$('.haha').html(data.result);
-				},'json');  */
-	});
-});
-</script>
-	<input type="text" id="my_name" name="my_name"/>
-	<button id="button">click me!</button>
-	<div class="haha"></div>
-<?php $this->load->view('global/footer'); ?>
-=======
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
 	<meta charset="utf-8">
 	<title>Test JQuery</title>
-    <?php echo $library_src; ?> 
-    <?php echo $script_foot; ?> 
-    <style>
-		.darkbar {
-			background-color: #252526;
-			}
-			
-		#topNavContainer{
-			height: 40px;
-			border-bottom-width: 1px;
-			border-bottom-style: solid;
-			border-bottom-color: #4b4b4c;
-			}		
-		#nav{
-			position:absolute;
-			bottom:10px;
-			right:10px;
-			z-index:6;
-			}
-		a{
-			color: #000000;
-			}
-		#slide-backgroup{
-			background-image:
-		}
-	</style>
+    <link type="text/css" rel="stylesheet" href="<?=base_url()?>_css/theme/v1/main.css" media="all" />
 </head>
-<body>
-	<div id="nav">
-		<div class="darkbar" id="topNavContainer">
-        <div id="container_16">
-        	<div id="topNavContent">
-            	<ui id="navMenu">
-                	<li class="menu-447"><a>haha</a></li>
-                </ui>
-            </div>
-        </div>
-    </div>
-    <div id="slide-backgroup">
-    </div>
-	<div id="button">Click me!</div>
+<body id="home">
+<!-- WRAPPER -->
+<div id="wrapper">
+
+<!-- MAIN -->
+<div class="main">
+
+<!-- HEAD -->
+<div class="slider">
+	<div class="slides">
+		
+		<div class="slide" style="display:block;">
+			<h1 class="title">Email delivery made easy</h1>
+			<p class="sub-title"><strong>"Improve your email deliverability and monitor action in real time."</strong></p>
+	
+			<p class="actions">
+				<a href="/signup" class="cta">
+					<span>
+						<strong>Get started now!</strong><br/>
+						<em>no engagement, no hidden fees</em>
+					</span>
+				</a>
+				<span class="learn-more">or <a href="http://www.mailjet.com/features" title="Cloud Emailing">Learn more</a></span>
+			</p>
+			
+			<p class="incentive"><img src="/images/theme/v1/en_US/bt_its_free_min.png" alt="Get started now!" /></p>
+		</div>
+        
+</div>
+</div>
+<!-- / HEAD -->
+
+<!-- TOP -->
+<div class="top">
+	<div class="col980">
+		<h2 class="title"><span>Mailjet</span> is a <span>real-time Cloud Emailing</span> platform: scalable, agile and flexible!</h2>
+		<p>Our unique algorithm boosts your deliverability and our platform provides in-depth insight so you can optimize more than ever.<br/>Because 20% of emails never reach the inbox Mailjet is a logical choice!		</p>
+	</div>
+</div>
+<!-- / TOP -->
+
+<div class="col980 top40" >
+
+</div>
+
+</div>
+<!-- / Main -->
+
+<!-- HEADER -->
+<div id="header-wrapper">
+<div id="header" class="col980" >
+
+	<p class="logo left">
+		<a href="http://www.mailjet.com/" title="Cloud Emailing">
+			<img src="<?=base_url()?>_images/theme/v1/icons/head_logo_tiny.png"  alt="Cloud Emailing" />
+			<span>Cloud Emailing</span>
+		</a>
+	</p>
+
+	<div id="menu" class="left">
+		<ul>
+			<li class="home"><a href="http://www.mailjet.com/" title="Home" >Home</a></li>
+			<li class="features"><a href="http://www.mailjet.com/features" title="Tour" >Tour</a></li>
+			<li class="pricing"><a href="http://www.mailjet.com/pricing" title="Pricing" >Pricing</a></li>
+			<li class="support">
+				<a href="http://www.mailjet.com/support" class="sub">Resources</a>				<ul>
+					<li><a href="http://www.mailjet.com/support" title="FAQ" >FAQ</a></li>
+					<li><a href="http://www.mailjet.com/docs" title="Documentation" >Documentation</a></li>
+					<li><a href="http://www.mailjet.com/docs/api" title="API" >API</a></li>
+					<li><a href="http://www.mailjet.com/plugin" title="Plugins" >Plugins</a></li>
+										<li><a href="http://www.mailjet.com/white-paper" title="White Papers" >White Papers</a></li>
+										<li><a href="http://blog.mailjet.com/" title="Blog" >Blog</a></li>
+				</ul>
+			</li>
+			<li class="about">
+				<a href="http://www.mailjet.com/About-us.htm" class="sub">About us</a>				<ul>
+					<li><a href="http://www.mailjet.com/About-us.htm" title="Company" >Company</a></li>
+					<li><a href="http://www.mailjet.com/contact" title="Contact" >Contact</a></li>
+					<li><a href="http://www.mailjet.com/Testimonials.htm" title="Customers" >Customers</a></li>
+				</ul>
+			</li>
+		
+		</ul>
+	</div>
+    
+	<ul id="languagedrop">
+		<li class="headlink">
+			<a href="#" class="en_US">Change country</a>
+			<ul>
+				<li><a href="https://eu.mailjet.com" hreflang="en_EU" class="en_EU">Europe (english)</a></li>
+				<li><a href="https://fr.mailjet.com" hreflang="fr" class="fr_FR">France</a></li>
+				<li><a href="https://www.mailjet.com" hreflang="en_US" class="en_US">USA</a></li>
+				<li><a href="https://uk.mailjet.com"  hreflang="en_UK" class="en_GB">United Kingdom</a></li>
+				<li><a href="https://de.mailjet.com"  hreflang="de" class="de_DE">Deutschland</a></li>
+				<li><a href="https://es.mailjet.com"  hreflang="es" class="es_ES">España</a></li>
+			</ul>
+		</li>
+	</ul>
+	
+	
+	<p id="register"><a href="https://www.mailjet.com/signin" title="Sign in" >Sign in</a><a href="https://www.mailjet.com/signup" class="signup">Sign Up for Free</a></p>
+
+	    
+</div>
+</div>
+<!-- / HEADER -->
+</div>
+<!-- / Wrapper -->
 </body>
 </html>
->>>>>>> Index Test
